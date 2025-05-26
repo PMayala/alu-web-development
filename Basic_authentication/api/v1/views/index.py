767bac6a8728cbe1a3d6f8ab4386895afa5a3d07
PMayala <p.mayala@alustudent.com>
2025-05-26 15:43:33 +0800

    commit

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
-    """GET /api/v1/unauthorized
+    """ GET /api/v1/unauthorized.
     Return:
-      - Unauthorized error.
+      - A 401 error by using abort.
     """
     abort(401)
 
 
-@app_views.route('/forbidden/', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> None:
-    """GET /api/v1/forbidden
+    """ GET /api/v1/forbidden.
     Return:
-      - Forbidden error.
+      - A 403 error by using abort.
     """
     abort(403)
